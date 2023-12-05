36cd1e759e7e623317cc6b690c1061cb5c18e84e
Jannusch Bigge <32895092+Jannusch@users.noreply.github.com>
2023-12-05 13:51:24 +0100

    week six
    week six

---
+++ tasks/week_six/main.py
@@ -0,0 +1,38 @@
+# Task 1
+# We need a little helper program to compare the power of our cars.
+# We have three different types of cars in our garage:
+#   SportCar with street license
+#   SportCar without street license
+#   Prototype car
+
+
+# Our cars have different attributes:
+#  hp: int
+#  zero_to_hundred: float
+#  weight: float
+#  max_speed: float
+
+
+# We want to compare the cars.
+# Therefore we calculate the acceleration and calculate the time it takes to drive 1000 meters.
+# The basic formula for the acceleration is:
+# a = v / t
+
+# The sport car with a street license has a weight of 1500kg and a max speed of 250km/h and 500hp and a zero to hundred time of 5 seconds.
+
+# The sport car without a street license has a weight of 1000kg and a max speed of 300km/h and 700hp and a zero to hundred time of 3.6 seconds.
+
+# The prototype car has a weight of 1000kg and a max speed of 330km/h and 1000hp and a zero to hundred time of 2.1 seconds.
+
+
+
+# Create a class for the cars.
+# Create a method which calculates the acceleration:
+# Create a method which calculates the distance for a given time.
+# Print the results in diagrams. (print what ever you want)
+# We simplify the problem. We use discrete values for the acceleration and the distance.
+
+
+# Task 2
+# The second task is to create a perceptron.
+# A perceptron has multiple inputs and one output.
